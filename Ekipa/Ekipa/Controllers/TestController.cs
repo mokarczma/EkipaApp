ed0180ea31905d8382aecafaa4354d29d573b750
DESKTOP-D1MTE9G\monia <monikakarczmarczukk@gmail.com>
2018-04-19 17:04:43 +0200

    Panel szukaj

---
+++ Ekipa/Ekipa/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Ekipa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ekipa.Controllers
+{
+    public class TestController : Controller
+    {
+        public List<SelectListItem> CitiesQuery()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                items = (from li in db.Cities
+                         select new SelectListItem
+                         {
+                             Text = li.Name,
+                             Value = li.ID.ToString()
+                         }).ToList();
+            }
+            return items;
+        }
+    }
+}
