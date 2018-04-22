c59ef498c4e487cd1c78d00676d66cd895c34377
DESKTOP-D1MTE9G\monia <monikakarczmarczukk@gmail.com>
2018-04-22 23:33:01 +0200

    Partial_Slider

--- Ekipa/Ekipa/Scripts/custom/custom.js
+++ Ekipa/Ekipa/Scripts/custom/custom.js
@@ -1,5 +1,9 @@
-﻿$(function () {
-    $('.datepicker').datepicker({
-        dateFormat: "dd-mm-yy"
-    });
-})
+﻿$('.images-slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 2,
+	arrows: false,
+	dots: true,
+	infinite: true,
+	variableWidth: true,
+});
+$('.single-item').slick();
